2e2921ea545b127df7785cfc58340ec0747dc695
Lara <lara.salman01@hotmail.com>
2025-02-12 20:10:04 +0100

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $request->validate([
+                "name" => "required",
+                "email" => "required|email|unique:users",
+                "password" => "required|confirmed|min:8",
+            ]);
+
+            User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => Hash::make($request->password),
+            ]);
+
+            return response()->json(["message" => "New user registered!"], 201);
+        } catch (\Exception $e) {
+            return response()->json(["message" => "Failed to register account"], 400);
+        }
+    }
+}
