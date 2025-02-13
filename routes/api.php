010de8cedf319d2ba1b9f577c22407401aaddf5b
Lara <lara.salman01@hotmail.com>
2025-02-13 02:08:25 +0100

    remove unecessary route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatbotController;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
